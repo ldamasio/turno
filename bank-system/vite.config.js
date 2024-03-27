8b238dfb395be11cf65ec9018abbc94ad81001d8
Leandro Damasio <ldamasio@gmail.com>
2024-03-27 03:58:36 -0300

    fix: configure trusted proxies

--- bank-system/vite.config.js
+++ bank-system/vite.config.js
@@ -3,7 +3,6 @@ import laravel from 'laravel-vite-plugin';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-    base: 'https://turno.leandrodamasio.com/',
     plugins: [
         laravel({
             input: 'resources/js/app.tsx',
